c9d626085797b8b47de4258bdc9c76cabf50a065
Veronica Lopez <nicaa0695@gmail.com>
2019-07-20 23:40:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,5 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/([0-9] *?){10} | (\([0-9]{3}\)(([0-9]{3}-[0-9]{4})| [0-9]{7})\b)/) ? true : false 
+  phone.match(/\(?\d{3}[) ]?\d{3}[- ]?\d{4}$/) ? true : false
 end
